a07781a82937d2a25d04e75abcfa7a65182528e5
DEVANSH <devanshb661@gmail.com>
2025-09-14 19:05:04 +0530

    Second Change

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,23 @@
-/** @type {import('tailwindcss').Config} */
-export default {
- content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      keyframes: {
-        rotate360: {
-          "0%": { transform: "rotate(0deg)" },
-          "100%": { transform: "rotate(360deg)" },
+  /** @type {import('tailwindcss').Config} */
+  export default {
+  content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+      extend: {
+        keyframes: {
+          spinSlow: {
+            '0%': { transform: 'rotate(0deg)' },
+            '100%': { transform: 'rotate(360deg)' },
+          },
         },
-      },
-      animation: {
-        "spin-slow": "rotate360 20s linear infinite",
-      },
+        animation: {
+          spinSlow: 'spinSlow 25s linear infinite', // slower rotation
+          spinFast: 'spinSlow 0.5s linear infinite', // super fast
+        },
     },
   },
-  plugins: [],
-}
+    plugins: [],
+  }
 
