f101f5d895c52fd57bc1b72108d952853d8f14f1
DEVANSH <devanshb661@gmail.com>
2025-09-15 09:36:48 +0530

    Changes

--- src/component/AboutUS/Aboutus.jsx
+++ src/component/AboutUS/Aboutus.jsx
@@ -4,7 +4,7 @@ export default function Aboutus() {
   return (
     <div className=' h-screen w-full bg-[#f4e6cc]  flex justify-center'>
       <div className=' w-[900px]'>
-        <p className=' font-[Arial]  font-bold bg-[#f4f4f4] rounded-xl shadow-lg text-2xl mt-5 text-center'>Impotant Points</p>
+        <p className=' font-[Arial]  font-bold bg-[#f4f4f4] rounded-xl shadow-lg text-2xl mt-5 text-center'>About Us</p>
       </div>
     </div>
   )
