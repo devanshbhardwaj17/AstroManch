3d41eb0b5854a4a9733e088a2fcb8bae7512b45e
DEVANSH <devanshb661@gmail.com>
2025-09-15 09:43:15 +0530

    Changes

--- src/component/AboutUS/Aboutus.jsx
+++ src/component/AboutUS/Aboutus.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Aboutus() {
   return (
-    <div className=' h-screen w-full bg-[#f4e6cc]  flex justify-center'>
+    <div className=' bg-[#f4e6cc] p-3 flex flex-wrap justify-center '>
       <div className=' w-[900px]'>
         <p className=' font-[Arial]  font-bold bg-[#f4f4f4] rounded-xl shadow-lg text-2xl mt-5 text-center'>About Us</p>
       </div>
